4fb5b70b59aba2e304b692bf308cf8d928fd6da9
ugesh <ugesh.roopchand@hotmail.com>
2018-06-18 14:04:45 +0400

    Project completed golive

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,11 @@
-var $ = require('jquery');
-var Person = require('./modules/Person');
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Person("Jane Smith", "green");
-jane.greet();
-
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
